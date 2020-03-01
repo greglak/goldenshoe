d39dda1a52602092faea14efc10e13538cda0278
Greg Lakatos <gergely_lakatos@yahoo.com>
2020-03-01 20:53:10 +0000

    Updates

---
+++ precache-manifest.a7a16b3c23c59e174c7f18cf4aeb7d84.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "796a99a39747a97f2bec",
+    "url": "/goldenshoe/static/css/main.58cfc45a.chunk.css"
+  },
+  {
+    "revision": "796a99a39747a97f2bec",
+    "url": "/goldenshoe/static/js/main.c53de0e1.chunk.js"
+  },
+  {
+    "revision": "8335bdb6784b3d605190",
+    "url": "/goldenshoe/static/js/runtime~main.dd4aa571.js"
+  },
+  {
+    "revision": "5c50a5bf61864a64c281",
+    "url": "/goldenshoe/static/css/2.35339135.chunk.css"
+  },
+  {
+    "revision": "5c50a5bf61864a64c281",
+    "url": "/goldenshoe/static/js/2.b94c5ba1.chunk.js"
+  },
+  {
+    "revision": "445c5f1239ce8dec2007ffc9e082b631",
+    "url": "/goldenshoe/static/media/product-1.445c5f12.png"
+  },
+  {
+    "revision": "d17648a1822800f811caeefee002dbfb",
+    "url": "/goldenshoe/static/media/product-2.d17648a1.png"
+  },
+  {
+    "revision": "8dd4b8537eb26ef29902b9f0b911a3e2",
+    "url": "/goldenshoe/static/media/product-3.8dd4b853.png"
+  },
+  {
+    "revision": "015d83e8cf46884324badc3da15372c0",
+    "url": "/goldenshoe/static/media/product-4.015d83e8.png"
+  },
+  {
+    "revision": "bf347ef803e7fbe7d8c9c854a425a75c",
+    "url": "/goldenshoe/static/media/product-5.bf347ef8.png"
+  },
+  {
+    "revision": "60deb0f943454566bc73fbcb742a1c14",
+    "url": "/goldenshoe/static/media/product-6.60deb0f9.png"
+  },
+  {
+    "revision": "34edbf9dd16041a0a7c240075cf6756a",
+    "url": "/goldenshoe/static/media/product-7.34edbf9d.png"
+  },
+  {
+    "revision": "3db949d199ea8777bcc0eb5e653bf44a",
+    "url": "/goldenshoe/static/media/product-8.3db949d1.png"
+  },
+  {
+    "revision": "0356fa58305497b02ac1ede3d205c8d4",
+    "url": "/goldenshoe/static/media/jumbotronBackground.0356fa58.jpg"
+  },
+  {
+    "revision": "f71188ddf8bd8f64fc959e5811a4917f",
+    "url": "/goldenshoe/static/media/logo.f71188dd.png"
+  },
+  {
+    "revision": "f132bed7b3d6b9c6f1b958e0fc343e35",
+    "url": "/goldenshoe/index.html"
+  }
+];
